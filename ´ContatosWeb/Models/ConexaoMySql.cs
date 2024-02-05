0c5c907f9ab5ef16f9491a37f6591291bd684783
Rafael RZ <63070361+Rodrigo40@users.noreply.github.com>
2024-02-05 14:15:18 +0200

    Add-ContatosModels Add.ConexalSql Add.Contos

---
+++ ´ContatosWeb/Models/ConexaoMySql.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace _ContatosWeb.Models
+{
+    public class ConexaoMySql
+    {
+        private static string ConexaoString = $"Data Source={Environment.UserDomainName}\\SQLEXPRESS02; Initial Catalog=sistema;Integrated Security=true";
+        public static string Conectar()
+        {
+            return ConexaoString;
+        }
+        public static string TestarConexao()
+        {
+            string resposta = string.Empty;
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConexaoString;
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+                resposta = "Conexão bem sucedida";
+            else
+                resposta = "Conexão mal sucedida";
+
+            return resposta;
+        }
+    }
+
+}
---
+++ ´ContatosWeb/Models/ContatoModels.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+
+namespace _ContatosWeb.Models
+{
+    public class ContatoModels
+    {
+        public List<Contatos> GetContatos()
+        {
+            SqlConnection con = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            List<Contatos> Lista = new List<Contatos>();
+            try
+            {
+                con.ConnectionString = ConexaoMySql.Conectar();
+                con.Open();
+
+                cmd.Connection = con;
+                cmd.CommandText = "select * from contato";
+                dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Contatos ct = new Contatos();
+                        ct.Id = Convert.ToInt32(dr["id"]);
+                        ct.Nome = dr["nome"].ToString();
+                        ct.Email = dr["email"].ToString();
+                        ct.Telefone = dr["telefone"].ToString();
+                        Lista.Add(ct);
+                    }
+                    dr.Close();
+                }
+            }
+            catch (SqlException)
+            {
+                con = null;
+                cmd = null;
+                dr = null;
+            }
+            return Lista;
+        }
+
+    }
+}
